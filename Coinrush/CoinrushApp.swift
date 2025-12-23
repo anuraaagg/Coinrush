c30ca33b2f71fc2bf8bdb302cf0d69c78e6cdba6
anuraaagg <s.anurag1998@gmail.com>
2025-12-23 15:49:19 +0530

    Initial Commit

---
+++ Coinrush/CoinrushApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinrushApp.swift
+//  Coinrush
+//
+//  Created by Anurag Singh on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CoinrushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
